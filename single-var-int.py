c77f84ae803c46c2c34ed9e96b659f2017b4c4b5
Adwait-Naravane <79789931+Adwait-Naravane@users.noreply.github.com>
2021-04-25 17:22:58 +0530

    added for single var

---
+++ single-var-int.py
@@ -0,0 +1,20 @@
+f = lambda x: x**2
+class SVF:
+  
+  def __init__(self, f):
+    self.f = f
+  
+  def value(self,x):
+    return self.f(x)
+  
+  def derivative(self, a, b, dx):
+      g = lambda x, m : (m(x+dx)-m(x))/dx   
+      return np.array([g(x,self.f) for x in np.arange(a,b,dx)])
+
+  def integrate(self, a,b,dx):
+    #trapezoidal rule
+    return sum([2*self.f(x) if not (x==a or x==b) else self.f(x) for x in np.arange(a,b+dx,dx)])*dx/2
+  def integral(self, a, x, dx):
+    return [SVF.integrate(self, a, i, dx) for i in np.arange(a,x, dx)]
+
+p = SVF(f)
